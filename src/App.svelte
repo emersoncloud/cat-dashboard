b0040629034750e50c7c0d8f05f151d0a8d648f1
Emerson Cloud <emerson.cloud@gmail.com>
2022-01-26 17:36:37 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	let name = 'puddle';
+    let cansFed = 0;
+
+    function feedHalfCan() {
+        cansFed = cansFed + 0.5;
+    }
+
+    function feedFullCan() {
+        cansFed = cansFed + 1;
+    }
+
+</script>
+
+<h1 id='one'>{name} eats food</h1>
+
+<button on:click={feedHalfCan}>half can</button>
+<button on:click={feedFullCan}>full can</button>
+
+<p>Food so far today: {cansFed}</p>
+<p>Goal: 2.5</p>
+
+<style>
+    #one {
+        color: blue;
+    }
+</style>
