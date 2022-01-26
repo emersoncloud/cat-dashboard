ea54ece31de3b16b5ea5990518d52636431ed087
kyel <kyletrusler@gmail.com>
2022-01-26 18:50:23 -0500

    v1

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,124 @@
 <script>
-	let name = 'puddle';
-    let cansFed = 0;
+  import { formatDistance, getDayOfYear, addDays } from "date-fns";
+  import FoodItem from "./FoodItem.svelte";
+  const HALF_CAN = 0.5;
+  const FULL_CAN = 1;
+  const CAN_GOAL = 2.5;
 
-    function feedHalfCan() {
-        cansFed = cansFed + 0.5;
+  let lastFed = Date.now();
+  let lastFedFriendly = formatDistance(lastFed, new Date(), {
+    addSuffix: true,
+  });
+  let cansFed = 0;
+  let foodEntries = [];
+
+  function feedCan(amount) {
+    cansFed += amount;
+    foodEntries = [
+      {
+        amount,
+        lastFed,
+      },
+      ...foodEntries,
+    ];
+    lastFed = Date.now();
+  }
+  setInterval(() => {
+    lastFedFriendly = formatDistance(lastFed, new Date(), { addSuffix: true });
+    if (getDayOfYear(lastFed) !== getDayOfYear(Date.now())) {
+      cansFed = 0;
     }
-
-    function feedFullCan() {
-        cansFed = cansFed + 1;
-    }
-
+  }, 1000 * 60);
 </script>
 
-<h1 id='one'>{name} eats food</h1>
+<div class="body" />
 
-<button on:click={feedHalfCan}>half can</button>
-<button on:click={feedFullCan}>full can</button>
+<div class="hero-time">
+  Last fed {lastFedFriendly}
+</div>
 
-<p>Food so far today: {cansFed}</p>
-<p>Goal: 2.5</p>
+<div class="flex">
+  <div class="food-button" on:click={() => feedCan(FULL_CAN)}>
+    <img id="half-can" alt="cat food can colored in" src={"/can-colored.png"} />
+    <span class="food-text">Full Can</span>
+  </div>
+  <div class="food-button" on:click={() => feedCan(HALF_CAN)}>
+    <img id="full-can" alt="cat food can outline" src={"/can.png"} />
+    <span class="food-text">Half Can</span>
+  </div>
+</div>
+
+<div class="food-total">
+  <div class="display-text">Food so far today: {cansFed}</div>
+  <div class="display-text" class:goal={cansFed >= CAN_GOAL}>
+    Goal: {CAN_GOAL}
+  </div>
+  <div class="food-list">
+    {#each foodEntries as { lastFed, amount } (lastFed)}
+      <FoodItem {lastFed} {amount} />
+    {/each}
+  </div>
+</div>
 
 <style>
-    #one {
-        color: blue;
-    }
+  .display-text {
+    font-size: 1.2em;
+  }
+  .goal {
+    background-color: rgb(108, 233, 104);
+  }
+  .food-list {
+    width: 300px;
+    height: 200px;
+    overflow-y: scroll;
+  }
+  .food-button {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+  }
+  .hero-time {
+    text-align: center;
+    font-size: 5em;
+  }
+  .food-text {
+    text-align: center;
+    font-size: 2em;
+  }
+
+  .food-total {
+    font-size: 2em;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  #half-can,
+  #full-can {
+    height: 20vh;
+  }
+  #one {
+    color: blue;
+  }
+
+  #heading {
+    display: flex;
+    justify-content: center;
+  }
+  .body {
+    background-image: url("/puddle.png");
+    background-repeat: repeat;
+    background-size: 50px 50px;
+    opacity: 20%;
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    z-index: -1;
+  }
+
+  .flex {
+    margin: 16px;
+    display: flex;
+    justify-content: space-evenly;
+  }
 </style>
---
+++ src/FoodItem.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let amount;
+  export let lastFed;
+  import { format } from "date-fns";
+  let src = amount === 0.5 ? "/can.png" : "/can-colored.png";
+</script>
+
+<div class="flex">
+  <img {src} alt="can" />
+  <span>{format(lastFed, "hh:mm do")}</span>
+</div>
+
+<style>
+  img {
+    height: 50px;
+  }
+  .flex {
+    align-items: center;
+    display: flex;
+    justify-content: space-evenly;
+  }
+</style>
